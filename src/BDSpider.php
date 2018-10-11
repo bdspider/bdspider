8e24ad57a5b01400bfc4783655ea1ddcde9b8c50
bdspider <44047842+bdspider@users.noreply.github.com>
2018-10-11 17:43:37 +0800

    Update BDSpider.php

--- src/BDSpider.php
+++ src/BDSpider.php
@@ -13,56 +13,75 @@ use BDSpider\Exceptions\SpiderException;
 class BDSpider
 {
 
+    /**
+     * @var GHttpClient 
+     */
+    private static $ghttpclient = null;
+
     const URL_SEARCH = 'https://www.baidu.com/s';
     const KEY_KEYWORD = 'wd';
     const KEY_PAGENUM = 'pn';
+    const ANALYSIS_SLEEP = 1;
 
+    public static function getGHttpClient()
+    {
+        if (is_null(self::$ghttpclient)) {
+            self::$ghttpclient = new GHttpClient();
+        }
+        return self::$ghttpclient;
+    }
+    
     public static function search($keyword, $max = 10)
     {
-        $result = [];
-
         $query = [
             self::KEY_KEYWORD => $keyword,
             self::KEY_PAGENUM => 0,
+            'rqlang'          => 'cn',
         ];
 
         $requestUrl = self::URL_SEARCH . '?' . http_build_query($query);
         // search by baidu.
         $links = QueryList::get($requestUrl)->rules([
-                'list' => ['.result.c-container>.t>a', ['href' => 'href', 'text' => 'text']],
+                'list' => ['.c-container .t a', ['href' => 'href', 'text' => 'text']],
             ])->range('body')->query()->getData()->all();
-
+        
         // list result.
         $searcResults = $links[ 0 ][ 'list' ] ?? [];
         if (!$searcResults) {
             throw new SpiderException('search error : no matched results.');
         }
-        
-        for ($i = 0; $i < 10; $i ++) {
-            if (!isset($searcResults[ $i ])) {
+        return self::analysisSearchResultRawUrls($searcResults, $max);
+    }
+
+    private static function analysisSearchResultRawUrls($urls, $max)
+    {
+        $result = $urls;
+        $num = 0;
+        foreach ($urls as $key => $url) {
+            $num ++;
+            if ($num > $max) {
                 break;
             }
-            if ($i >= $max) {
-                break;
+            if (!isset($url['href'])) {
+                unset($result[$key]);
+                continue;
             }
-            $spoofinglink = $searcResults[ $i ];
-            $href = $spoofinglink[ 'href' ];
-            $client = new GHttpClient();
-            $realRequestOption = [
-                GROption::ALLOW_REDIRECTS => false
-            ];
-            // sleep 1.
-            sleep(1);
-            $response = $client->request('GET', $href, $realRequestOption);
-            $location = $response->getHeader('Location');
-            $reallink = $location[0] ?? $href;
-            
-            $result[$i] = [
-                'text' => $spoofinglink['text'],
-                'href' => $reallink,
-            ];
+            sleep(self::ANALYSIS_SLEEP);
+            $url[ 'href' ] = self::getRealUrl($url[ 'href' ]);
+            $result[ $key ] = $url;
         }
-        
-        return $result;
+        return array_values($result);
+    }
+
+    public static function getRealUrl($href)
+    {
+        $client = self::getGHttpClient();
+        $realRequestOption = [
+            GROption::ALLOW_REDIRECTS => false
+        ];
+        $response = $client->request('GET', $href, $realRequestOption);
+        $location = $response->getHeader('Location');
+        $reallink = $location[ 0 ] ?? $href;
+        return $reallink;
     }
 }
